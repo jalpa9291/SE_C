522fd475d1b9bd37aeed124aa4f22696efe660eb
Jalpa Vadgama <jalpavadgama52@gmail.com>
2023-12-02 10:05:53 +0530

    update

---
+++ 4addsub.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+	int a=67, b=34;
+	
+	cout << "The addition of a and b is " << a+b << endl;
+	cout << "The substraction of a and b is " << a-b << endl;
+	cout << "The Multiplication of a and b is " << a*b << endl;
+	cout << "The division of a and b is " << a/b;
+	
+}
