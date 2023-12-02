ef2263a98aab013833ae8c5a8a4cd1e096a42c47
Jalpa Vadgama <jalpavadgama52@gmail.com>
2023-12-02 09:59:25 +0530

    update

---
+++ 3variable.cpp
@@ -0,0 +1,10 @@
+#include<iostream>		// header file
+
+using namespace std;	// standard library for using standard names	
+
+main()		// main function
+{
+	int a=50;
+//	cout << a;
+	cout << "The value of a is " << a;
+}
